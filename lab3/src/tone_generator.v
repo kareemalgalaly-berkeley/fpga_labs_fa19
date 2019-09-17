b30d6575dca72af09dc0e4c6c0748a49438baa1b
Class Account <eecs151-acn@c125m-17.EECS.Berkeley.EDU>
2019-09-17 11:31:49 -0700

    asdf

--- lab3/src/tone_generator.v
+++ lab3/src/tone_generator.v
@@ -7,19 +7,26 @@ module tone_generator (
     output square_wave_out,
     output [32:0] _counter
 );
-    reg [32:0] counter = 32'h0000_0001; // true min is 20 for the default settings
+    reg [32:0] counter = 32'h0000_0000; // true min is 20 for the default settings
     reg pwm_out = 0;
 
     assign square_wave_out = pwm_out;
     assign _counter = counter;
 
+    if (counter = 32'h0000_0000) begin
+        counter = tone_switch_period;
+    end
+
     always @(posedge clk or negedge clk) begin
-        if (counter == tone_switch_period) begin
+        //if (counter == tone_switch_period) begin
+        if (counter == 32'h0000_0001) begin
             pwm_out <= ~pwm_out;
-            counter <= 32'h0000_0001; //tone_switch_period;
+            //counter <= 32'h0000_0001;
+            counter <= tone_switch_period;
             //counter <= 284091;
         end else begin
-            counter <= counter + 1;
+            //counter <= counter + 1;
+            counter <= counter - 1;
         end
     end
 endmodule
