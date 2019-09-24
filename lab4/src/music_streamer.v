e6ef82be803c778e8424d13f4f5163dd02dc0c8e
Kareem Ahmad <kareemahmad@pop-os.localdomain>
2019-09-24 09:16:41 -0700

    cleaning

--- lab4/src/music_streamer.v
+++ lab4/src/music_streamer.v
@@ -6,9 +6,23 @@ module music_streamer (
     input play_pause,
     input reverse,
     output [2:0] leds,
-    output [23:0] tone
+    output reg [23:0] tone
 );
-    // Instantiate the ROM here
-    assign leds = 0;
-    assign tone = 0;
+    // Instantiations
+    reg [22:0] counter = 0;
+
+    // ROM 
+    wire [9:0] rom_addr;
+    wire [23:0] rom_data;
+    wire [9:0] rom_ladd;
+    rom musicrom(.address(rom_addr), .data(rom_data), .last_address(rom_ladd));
+
+    // main
+    always @(posedge clk) begin
+        if (counter >= 23'd5_000_000) begin
+            counter <= 0;
+            tone <= rom_data;
+        end
+        else counter <= counter + 1;
+    end
 endmodule
