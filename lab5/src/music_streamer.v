7d85f0666a53c1b09768b9a6169a05540882224f
Kareem Ahmad <kareemahmad@pop-os.localdomain>
2019-10-13 14:55:32 -0700

    premergePOP

--- lab5/src/music_streamer.v
+++ lab5/src/music_streamer.v
@@ -10,6 +10,12 @@ module music_streamer (
 );
     // Instantiations
     reg [22:0] counter = 0;
+    reg [1:0] state = 0; // 0 : paused , 01 : play, 11 : reverse_play
+    
+    // Display
+    assign leds[0] = state == 2'b01; // play
+    assign leds[1] = !state[0];      // paused
+    assign leds[0] = state[1];       // reverse
 
     // ROM 
     reg [9:0] rom_addr = 0;
@@ -22,7 +28,11 @@ module music_streamer (
         if (counter >= 23'd5_000_000) begin
             counter <= 0;
             tone <= rom_data;
-            rom_addr <= rom_addr + 1;
+            case (rom_addr)
+                0        : rom_addr <= rom_ladd;
+                rom_ladd : rom_addr <= 0;
+                default  : begin if rom_addr <= rom_ladd + 1;
+            endcase
         end
         else counter <= counter + 1;
     end
