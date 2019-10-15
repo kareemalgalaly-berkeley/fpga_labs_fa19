e901373047c8f51e05e8f81875a451b395a7d843
Class Account <eecs151-acn@c125m-5.EECS.Berkeley.EDU>
2019-10-15 12:14:03 -0700

    fifo start

--- lab6/src/fifo.v
+++ lab6/src/fifo.v
@@ -12,10 +12,47 @@ module fifo #(
 
     // Read side
     input rd_en,
-    output [data_width-1:0] dout,
+    output reg [data_width-1:0] dout,
     output empty
 );
-    assign full = 1'b1;
-    assign empty = 1'b0;
-    assign dout = 0;
+    //assign full = 1'b1;
+    //assign empty = 1'b0;
+    //assign dout = 0;
+
+    // Initialization
+    reg [addr_width-1:0] write_pointer = 0;
+    reg [addr_width-1:0] read_pointer = 0; 
+    reg [data_width-1:0] data [fifo_depth-1:0];
+
+    reg _full = 0;
+
+    // Always
+    //assign dout = data[read_pointer];
+    assign full = _full;
+    assign empty = (write_pointer == read_pointer) && !(full);
+
+    always @(posedge clk) 
+        if (rst) begin
+            genvar i;
+            generate
+                for (i = 0; i < fifo_depth; i = i + 1) begin : genreset
+                    data[i] = addr_width'b0;
+                end
+            endgenerate
+        end else begin
+            // main
+            if (wr_en && !full) begin
+                write_pointer <= write_pointer + 1;
+                data[write_pointer] <= din;
+                if ((write_pointer + 1) == read_pointer) _full <= 1'b1;
+                else _full <= 1'b0;
+            end else if (rd_en && !empty) begin
+                read_pointer <= read_pointer + 1;
+                dout <= data[read_pointer];
+                _full <= 1'b0;
+            end
+        end
+    end
+
+
 endmodule
